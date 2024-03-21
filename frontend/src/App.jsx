fc79e02310f9c92e2fe709764d5ef79389fece35
ayeshafirdaus <ayeshafirdaus21@gmail.com>
2024-03-21 10:33:55 +0530

    3rd commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import GlobalStyles from './styles/GlobalStyles'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <GlobalStyles />
+      <div>
+        
+      </div>
+  
+    </div>
+  )
+}
+
+export default App
