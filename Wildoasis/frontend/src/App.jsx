f125214e67be427fd070974eacfc2031cc0a9069
ayeshafirdaus <ayeshafirdaus21@gmail.com>
2024-03-21 12:44:16 +0530

    "3rd commit"

--- Wildoasis/frontend/src/App.jsx
+++ Wildoasis/frontend/src/App.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
-import styled  from "styled-components"
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+
 import GlobalStyles from './styles/GlobalStyles'
-import Heading from "./ui/Heading";
-export default function App() {
+import Dashboard from './Pages/Dashboard'
+import Account from './Pages/Account'
+import Settings from './Pages/Settings'
+import Bookings from './Pages/Bookings'
+import Users from './Pages/Users'
+import PageNotFound from './Pages/PageNotFound'
+import Cabins from './Pages/Cabins'
+import AppLayout from './ui/AppLayout'
+import Login from './Pages/Login'
+
+function App() {
+
+
   return (
     <>
     <GlobalStyles />
-    <div>
-    <Heading as="h1">hello</Heading>
-   </div>
+    <BrowserRouter>
+    <Routes>
+      <Route element={<AppLayout />} >
+    <Route index element={<Navigate replace to="dashboard"/>}/>
+      <Route path='dashboard' element={<Dashboard />}/>
+      <Route path='account' element={<Account />}/>
+      <Route path='Cabins' element={<Cabins />}/>
+
+      <Route path='settings' element={<Settings />}/>
+      <Route path='bookings' element={<Bookings />}/>
+      <Route path='users' element={<Users />}/>
+      </Route>
+      <Route path='login' element={<Login />}/>
+      <Route path='*' element={<PageNotFound />}/>
+
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
